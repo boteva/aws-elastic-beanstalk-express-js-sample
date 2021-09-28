d8a38b38721ed7405f6d00025c01b955946f5597
boteva <40807299+boteva@users.noreply.github.com>
2021-09-28 21:38:35 +0300

    Testing pipeline

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Sunny day'));
+app.get('/', (req, res) => res.send('Another Sunny day!'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
